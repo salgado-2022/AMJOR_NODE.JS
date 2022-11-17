bf353690cfe6fcc44d1413bb1bc36e159932abf5
nini 🌹 <86623637+SantiagoMesa0911@users.noreply.github.com>
2022-11-17 09:13:17 -0500

    arreglo del js

--- public/assets/js/validarRegistro.js
+++ public/assets/js/validarRegistro.js
@@ -6,13 +6,13 @@ let confirmarContrasena = document.querySelector('#confirmarContrasena').value
 
 if(nombre == "" || correo == "" || contrasena == ""){
     alert('Complete todos los campos del formulario!')
-}
-
-if(contrasena != confirmarContrasena){
+}else if(contrasena != confirmarContrasena){
     alert('Las contraseñas no coinciden!')
 }else{
     alert('Registrado exitosamente!')
 }
+
+
 
 
 }
