8847e1448e8fc9193579a380d5cfb10d476043fc
Dayanna <dayannalaitonp@gmail.com>
2022-11-22 07:33:08 -0500

    hola 22/11

--- public/assets/js/anchetas.js
+++ public/assets/js/anchetas.js
@@ -1,30 +1,14 @@
-var data = {
-    items: [
-        {
-            title: "Hello",
-            description: "Hello Text",
-            image: "https://via.placeholder.com/300x100",
-        },
-        {
-            title: "Card title",
-            description: "Some quick example text to build on the card title and make up the bulk of the card's content.<p>Another paragraph</p><p>Another paragraph</p>",
-            image: "https://via.placeholder.com/300x100",
-        }
-    ]
-};
 
-var source = document.getElementById("card-template").innerHTML;
-var template = Handlebars.compile(source);
-var html = template(data);
+const camposAncheta = () =>{
+    let nombre = document.querySelector('#nombreAncheta').value
+    let descripcion = document.querySelector('#descAncheta').value 
 
-document.getElementById("content").innerHTML = html;
-console.log(html);
+    if (nombre == "" || descripcion == "") {
+        alert('Por favor llene todos los campos obligatorios, es decir, los que están marcados con un *')
+    }else{
+        alert('Ancheta agregada correctamente.')
+    }
+}
 
-//jquery method
-$("#content .card-footer .btn").on("click", function (event) {
-    console.log("jquery");
-
-    var index = $(".card").index(event.currentTarget);
-    console.log("index = " + index);
-    console.log(event.target.dataset.target);
-});
+const botonVCarrito = document.querySelector('#botonCarrito')
+botonVCarrito.addEventListener('click',validarCarrito)
