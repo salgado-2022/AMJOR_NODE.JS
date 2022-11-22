ee598f46c24b03c0103b7dd89831096b9473a8b6
nini 🌹 <86623637+SantiagoMesa0911@users.noreply.github.com>
2022-11-22 07:18:15 -0500

    cambios de clase de magno de nia

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config
 const express = require ('express')
 const hbs = require('hbs')
 const app = express()
-const port = process.env.PORT 
+const port = process.env.PORT || 4000
 
 app.use(express.static('public'))
 
