ec1468fd68e755ac2d724495280671097310aa79
Stephannia Garcia <dsgarcia815@misena.edu.co>
2022-11-27 15:51:32 -0500

    validaciones de acceso

--- public/assets/js/validarRegistro.js
+++ public/assets/js/validarRegistro.js
@@ -12,10 +12,41 @@ if(nombre == "" || correo == "" || contrasena == ""){
     alert('Registrado exitosamente!')
 }
 
-
-
-
 }
+
+const nombre = document.getElementById("nombre")
+const correo = document.getElementById("correo")
+const contrasena = document.getElementById("contrasena")
+const confirmarContrasena = document.getElementById("confirmarContrasena")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", x=>{
+    x.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if(nombre.value.length <6){
+        warnings += `El nombre es demasiado corto <br>`
+        entrar = true
+    }
+
+    console.log(regexEmail.test(correo.value))
+    if(!regexEmail.test(correo.value)){
+        warnings += `El correo no es valido <br>`
+        entrar = true
+    }
+
+    if(contrasena.value.length < 5){
+        warnings += `La contraseña es muy corta <br>`
+        entrar = true
+    }
+    
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }
+} )
 
 const boton = document.querySelector('#botonRegistrar')
 boton.addEventListener('click', validar)
